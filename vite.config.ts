5dd127570b87e839d6cc057070daded9ce59f223
Anton Skybytskyi <anton.skybytskyi@onairentertainment.com>
2023-02-03 10:57:03 +0200

    Init SolidJS project with prisma and tailwindcss

---
+++ vite.config.ts
@@ -0,0 +1,18 @@
+import solid from "solid-start/vite";
+import { defineConfig } from "vite";
+import devtools from "solid-devtools/vite"
+
+export default defineConfig({
+  plugins: [
+    devtools({
+      autoname: true,
+      locator: {
+        targetIDE: "vscode",
+        componentLocation: true,
+        jsxLocation: true,
+      },
+    }),
+    solid(),
+  ],
+  ssr: { external: ["@prisma/client"] },
+});
